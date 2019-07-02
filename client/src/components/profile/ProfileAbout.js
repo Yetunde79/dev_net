49bb3d0f7ec82096f4474a1c15f503b394290bfc
Yetunde Sola-Adebayo <yetundesola-adebayo@Yetundes-Air.attlocal.net>
2019-07-02 17:03:19 -0500

    added ProfileAbout and ProfileCreds component

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,37 +1,35 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+    const firstName = profile.user.name.trim().split(" ")[0];
+
+    //skills list
+    const skills = profile.skills.map((skill, index) => (
+      <div className="p-3">
+        <i className="fa fa-check" /> {skill}
+      </div>
+    ));
+
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="card card-body bg-light mb-3">
-            <h3 className="text-center text-info">John's Bio</h3>
-            <p className="lead">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Provident fuga cum necessitatibus blanditiis vel, officia facere
-              porro esse numquam assumenda doloremque saepe aliquam nemo
-              excepturi aliquid maiores! Excepturi, libero repudiandae.
-            </p>
-            <hr />
+            {isEmpty(profile.bio) ? null : (
+              <React.Fragment>
+                <h3 className="text-center text-info">{firstName}'s Bio</h3>
+                <p className="lead">{profile.bio}</p>
+                <hr />
+              </React.Fragment>
+            )}
+
             <h3 className="text-center text-info">Skill Set</h3>
             <div className="row">
               <div className="d-flex flex-wrap justify-content-center align-items-center">
-                <div className="p-3">
-                  <i className="fa fa-check" /> HTML
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> CSS
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> JavaScript
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> Python
-                </div>
-                <div className="p-3">
-                  <i className="fa fa-check" /> C#
-                </div>
+                {skills}
               </div>
             </div>
           </div>
