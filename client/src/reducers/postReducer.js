8b022fcab5cef7919cdfa84d8cadcf722c6d052f
Yetunde Sola-Adebayo <yetundesola-adebayo@Yetundes-Air.attlocal.net>
2019-07-04 10:03:48 -0500

    added postAction

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,47 @@
+import {
+  POST_LOADING,
+  GET_POSTS,
+  GET_POST,
+  ADD_POST,
+  DELETE_POST
+} from "../actions/types";
+
+const initialState = {
+  post: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        post: null
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        post: null
+      };
+    default:
+      return state;
+  }
+}
