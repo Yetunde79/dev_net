47f8eb3aa657cc1cf7d7f85de9995d4bfa0b5f00
Yetunde Sola-Adebayo <yetundesola-adebayo@Yetundes-Air.attlocal.net>
2020-04-02 09:58:03 -0500

    changed github api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
-const request = require("request");
 const axios = require("axios");
 
 const config = require("config");
@@ -266,30 +265,19 @@ router.delete(
 
 router.get("/github/:username", async (req, res) => {
   try {
-    const options = {
-      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`,
-      method: "GET",
-      headers: {
-        "user-agent": "node.js",
-        Authorization: `token ${config.get("githubToken")}`
-      }
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      "user-agent": "node.js",
+      Authorization: `token ${config.get("githubToken")}`
     };
 
-    console.log(config.get("githubToken"));
-    console.log("hi");
-
-    await request(options, (error, response, body) => {
-      if (error) console.error(error);
-
-      if (response.statusCode !== 200) {
-        return res.status(404).json({ msg: "No Github Profile" });
-      }
-
-      res.json(JSON.parse(body));
-    });
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    return res.status(404).json({ msg: "No Github profile found" });
   }
 });
 
